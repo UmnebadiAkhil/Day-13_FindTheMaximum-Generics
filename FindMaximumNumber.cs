e70782ec8a35925e50f7a7a212cad303512297a7
Akhil <akhilu5042@gmail.com>
2022-01-21 00:22:42 +0530

    FindMaximumNumber.cs

---
+++ FindMaximumNumber.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximumByGenerics
+{
+    public class FindMaximumNumber<T> where T: IComparable
+    {
+        public static T MaximumIntNumbers(T FirstValue, T SecondValue, T ThirdValue)
+        {
+            if (FirstValue.CompareTo(SecondValue) > 0 && FirstValue.CompareTo(ThirdValue) > 0 ||
+                FirstValue.CompareTo(SecondValue) >= 0 && FirstValue.CompareTo(ThirdValue) >= 0 ||
+                FirstValue.CompareTo(SecondValue) > 0 && FirstValue.CompareTo(ThirdValue) >= 0)
+            {
+                Console.WriteLine(FirstValue);
+                return FirstValue;
+            }
+            if (SecondValue.CompareTo(FirstValue) > 0 && SecondValue.CompareTo(ThirdValue) > 0 ||
+                SecondValue.CompareTo(FirstValue) >= 0 && SecondValue.CompareTo(ThirdValue) >= 0 ||
+                SecondValue.CompareTo(FirstValue) > 0 && SecondValue.CompareTo(ThirdValue) >= 0)
+            {
+                Console.WriteLine(SecondValue);
+                return SecondValue;
+            }
+            if (ThirdValue.CompareTo(FirstValue) > 0 && ThirdValue.CompareTo(SecondValue) > 0 ||
+                ThirdValue.CompareTo(FirstValue) >= 0 && ThirdValue.CompareTo(SecondValue) >= 0 ||
+                ThirdValue.CompareTo(FirstValue) > 0 && ThirdValue.CompareTo(SecondValue) >= 0)
+            {
+                Console.WriteLine(ThirdValue);
+                return ThirdValue;
+            }
+            return FirstValue;
+        }
+
+    }
+}
